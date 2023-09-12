d6568069b06be1f1ebfde88f31592d773c5d0a61
Adarsh Kumar Sahu <106156145+adarshsahu27@users.noreply.github.com>
2023-09-13 01:03:03 +0530

    initial push with signin signup component

---
+++ src/components/SignIn/SignIn.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function SignInForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+    setEmailError('');
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+    setPasswordError('');
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!email) {
+      setEmailError('Email is required');
+      return;
+    }
+
+    if (!password) {
+      setPasswordError('Password is required');
+      return;
+    }
+
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (!emailPattern.test(email)) {
+      setEmailError('Invalid email format');
+      return;
+    }
+    console.log('Login successful');
+  };
+
+  return (
+    <div className='card'>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={handleEmailChange}
+          />
+          <span style={{ color: 'red' }}>{emailError}</span>
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <span style={{ color: 'red' }}>{passwordError}</span>
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+      <p>Don't have an account? <Link to="/signup">Signup here</Link></p>
+    </div>
+  );
+}
+
+export default SignInForm;
---
+++ src/components/SignUp/SignUp.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+function SignupForm() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    name: "",
+    number: "",
+    gender: "",
+    address: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="card">
+      <h2>Signup</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Name:</label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Number:</label>
+          <input
+            type="text"
+            name="number"
+            value={formData.number}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Gender:</label>
+          <input
+            type="text"
+            name="gender"
+            value={formData.gender}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Address:</label>
+          <input
+            type="text"
+            name="address"
+            value={formData.address}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <button type="submit">Signup</button>
+        </div>
+      </form>
+      <p>
+        Already have an account? <Link to="/">Login here</Link>
+      </p>
+    </div>
+  );
+}
+
+export default SignupForm;
